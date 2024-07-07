f6acf28af01d52e824f3ba3d11ec0ab008b47c72
Ian Kirui <144064407+kiruiian@users.noreply.github.com>
2024-07-07 11:22:01 +0300

    Add files via upload

---
+++ Fibonacci Sequence/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Fibonacci_Sequence
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.lblInput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(130, 40);
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(283, 20);
+            this.txtInput.TabIndex = 0;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(191, 84);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(126, 23);
+            this.btnGenerate.TabIndex = 1;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(80, 136);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(35, 13);
+            this.lblResult.TabIndex = 2;
+            this.lblResult.Text = "label1";
+            // 
+            // lblInput
+            // 
+            this.lblInput.AutoSize = true;
+            this.lblInput.Location = new System.Drawing.Point(46, 47);
+            this.lblInput.Name = "lblInput";
+            this.lblInput.Size = new System.Drawing.Size(69, 13);
+            this.lblInput.TabIndex = 3;
+            this.lblInput.Text = "Enter n terms";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(425, 182);
+            this.Controls.Add(this.lblInput);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.txtInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Label lblInput;
+    }
+}
+
---
+++ Fibonacci Sequence/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fibonacci_Sequence
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtInput.Text, out int n) && n >= 0)
+            {
+                string result = GenerateFibonacci(n);
+                lblResult.Text = result;
+            }
+            else
+            {
+                lblResult.Text = "Please Enter a real integer!";
+            }
+        }
+        private string GenerateFibonacci(int n)
+        {
+            if (n == 0) return "0";
+            if (n == 1) return "1";
+            int a = 0, b = 1, next;
+            string sequence = "0,1";
+
+            for (int i = 2; i <= n; i++)
+            {
+                next = a + b;
+                sequence += "," + next;
+                a = b;
+                b = next;
+            }
+            return sequence;
+        }
+    }
+}
